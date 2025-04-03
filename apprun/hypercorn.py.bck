95c0825fc6c2a8609bb542ea9d6c9b47907c3e85
zhangjun <ibmzhangjun@139.com>
2025-04-03 16:45:31 +0800

    docker build

--- apprun/hypercorn.py.bck
+++ apprun/hypercorn.py.bck
@@ -3,12 +3,12 @@
 
 loglevel = "debug"
 workers = 4
-bind = "0.0.0.0:8943"
-insecure_bind = "0.0.0.0:8980"
+bind = "0.0.0.0:8843"
+insecure_bind = "0.0.0.0:8880"
 graceful_timeout = 120
 worker_class = "uvloop"
 keepalive = 5
-errorlog = "/Users/zhangjun/PycharmProjects/CapricornusNG/backend/log/hypercorn_error.log"
-accesslog = "/Users/zhangjun/PycharmProjects/CapricornusNG/backend/log/hypercorn_access.log"
-keyfile = "/Users/zhangjun/PycharmProjects/CapricornusNG/backend/cert/key.pem"
-certfile = "/Users/zhangjun/PycharmProjects/CapricornusNG/backend/cert/cert.pem"
+errorlog = "/Users/zhangjun/PycharmProjects/crtool/backend/log/hypercorn_error.log"
+accesslog = "/Users/zhangjun/PycharmProjects/crtool/backend/log/hypercorn_access.log"
+keyfile = "/Users/zhangjun/PycharmProjects/crtool/cert/key.pem"
+certfile = "/Users/zhangjun/PycharmProjects/crtool/cert/cert.pem"
--- docker/bd_build/50_start_h.sh
+++ docker/bd_build/50_start_h.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-FIND_FILE="/opt/crtool/backend/appconfig/hypercorn.py"
+FIND_FILE="/opt/crtool/apprun/hypercorn.py"
 FIND_STR="workers"
 if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
     echo "hypercorn config exist"
 else
-    cp /opt/crtool/backend/appconfig/hypercorn_default.py /opt/crtool/backend/appconfig/hypercorn.py
+    cp /opt/crtool/apprun/hypercorn_default.py /opt/crtool/apprun/hypercorn.py
 fi
 
-FIND_DB_FILE="/opt/crtool/backend/appconfig/hypercorn.py"
-if [ ! -f "$FIND_DB_FILE" ]; then
-    echo "sample database exist"
-else
-    cp /opt/crtool/backend/crud/sample.db /opt/crtool/backend/data/
-fi
 cd /opt/crtool/backend && \
-nohup /opt/crtool/backend/venv/bin/hypercorn -c /opt/crtool/backend/appconfig/hypercorn.py main:app >> /tmp/crtool.log 2>&1 &
+nohup /opt/crtool/venv/bin/hypercorn -c /opt/crtool/apprun/hypercorn.py main:app >> /tmp/crtool.log 2>&1 &
