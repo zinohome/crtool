d97c70dbeea1816c117076dc33ed86666719a9ab
zhangjun <ibmzhangjun@139.com>
2025-05-25 11:11:53 +0800

    docker build

--- backend/apps/admin/pages/changerequestadmin.py
+++ backend/apps/admin/pages/changerequestadmin.py
@@ -547,7 +547,7 @@ class ChangerequestAdmin(SwiftAdmin):
         user = await auth.get_current_user(request)
         if not bulk:
             # 构建主表Create
-            formtab = amis.Tabs(tabsMode='simple')
+            formtab = amis.Tabs(tabsMode='strong')
             formtab.tabs = []
             fieldlist = []
             requirelist =[]
