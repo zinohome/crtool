38252edb66a1ad671dffe5496a70a423fbfb3105
zhangjun <ibmzhangjun@139.com>
2025-05-25 09:59:55 +0800

    docker build

--- backend/utils/mailtool.py
+++ backend/utils/mailtool.py
@@ -39,6 +39,28 @@ class MailTool(object):
             log.debug(f"邮件发送失败：{e}")
             traceback.print_exc()
 
+    def send_outlook_email(self,receiver_email, subject, body):
+        # 创建邮件对象
+        msg = MIMEMultipart()
+        msg["From"] = self._sender_email
+        msg["To"] = receiver_email
+        msg["Subject"] = subject
+        msg.attach(MIMEText(body, "plain"))
+        try:
+            # 连接到SMTP服务器
+            smtp = smtplib.SMTP(self._smtp_server, self._smtp_port, timeout=300)
+            smtp.ehlo()
+            smtp.starttls()  # 启用TLS加密
+            smtp.ehlo()
+            (code, resp) = smtp.login(self._sender_email, self._sender_password)
+            smtp.send_message(msg)
+            smtp.quit()
+            log.debug("邮件发送成功！")
+        except Exception as e:
+            log.debug(f"邮件发送失败：{e}")
+            traceback.print_exc()
+
+
 if __name__ == '__main__':
     mailtool = MailTool()
     mailtool.send_email("Jun.Zhang1@ibm.com","crtool测试邮件","这是一封测试邮件，发送自 Python。")
